64d353e1ffdf42858bcb5c4fdc9695113546b800
Konovalenko Pavel <konovalenkopavel@gmail.com>
2022-12-23 20:15:10 +0300

    оптимизация сохранения параметров в url

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -13,6 +13,8 @@ import qs from "qs";
 import { useHistory } from "react-router-dom";
 
 const Home = ({ searchValue }) => {
+  const isSearch = React.useRef(false); // после первого рендера и изменении параметров сортировки меняется на true
+  const isMounted = React.useRef(false);
   const { selectedSort, activeCategory } = useSelector((state) => state.filter);
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
@@ -39,19 +41,7 @@ const Home = ({ searchValue }) => {
     { name: "алфавиту Asc", sortProperty: "-title" },
   ];
 
-  React.useEffect(() => {
-    if (window.location.search) {
-      const params = qs.parse(window.location.search.substring(1));
-      console.log(params, "params");
-      const sort = sortTypes.find(
-        (obj) => obj.sortProperty === params.sortProperty
-      );
-      console.log(sort, "sort");
-      dispatch(setFilters({ ...params, sort }));
-    }
-  }, []);
-
-  React.useEffect(() => {
+  const fetchPizzaz = () => {
     const sortApi = selectedSort.sortProperty.replace("-", "");
     const sortApiType = selectedSort.sortProperty.includes("-")
       ? "asc"
@@ -70,21 +60,51 @@ const Home = ({ searchValue }) => {
       });
 
     window.scrollTo(0, 0);
+  };
+
+  // отправить в redux, если при первом рендере забиты поисковые параметры
+  React.useEffect(() => {
+    // window.location.search возвращает поисковые параметры, включая "?" и далее
+    if (window.location.search) {
+      const params = qs.parse(window.location.search.substring(1));
+
+      const sort = sortTypes.find(
+        (obj) => obj.sortProperty === params.sortProperty
+      );
+      dispatch(setFilters({ ...params, sort }));
+    }
+    isSearch.current = true;
+  }, []);
+
+  // основной рендер, направляющий запросы на сервер при изменении параметров сортировки
+  React.useEffect(() => {
+    if (isSearch.current) {
+      fetchPizzaz();
+    }
+    isSearch.current = true;
   }, [activeCategory, selectedSort, searchValue, currentPage]);
 
+  // если изменили параметры и был первый рендер
+  React.useEffect(() => {
+    //  если был первый рендер, то вбивай данные в url
+    if (isMounted.current) {
+      // создаем строку с по типу api запроса, например: sortProperty=rating&activeCategory=3&currentPage=1
+      const queryString = qs.stringify({
+        sortProperty: selectedSort.sortProperty,
+        activeCategory,
+        currentPage,
+      });
+      // задаем url полученный от queryString путь
+      navigate.push(`?${queryString}`);
+    }
+
+    isMounted.current = true; // меняем на true после первого рендера
+  }, [activeCategory, selectedSort, currentPage]);
+
+  // при изменении параметров сортировки менять текущую страницу на первую
   React.useEffect(() => {
     dispatch(setCurrentPage(1));
   }, [activeCategory, selectedSort, searchValue]);
-
-  React.useEffect(() => {
-    const queryString = qs.stringify({
-      sortProperty: selectedSort.sortProperty,
-      activeCategory,
-      currentPage,
-    });
-
-    navigate.push(`?${queryString}`);
-  }, [activeCategory, selectedSort, currentPage]);
 
   return (
     <div className="container">
